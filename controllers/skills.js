7c212415ea1cc1f554088d656bee1c787fb6c04e
dani-diaz <daniela.diaz0808@gmail.com>
2022-04-12 22:12:24 -0700

    part one done

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,10 +1,17 @@
 const Skill = require('../models/skill');
 
 module.exports = {
-    index
+    index,
+    show
 }
 
 function index(req, res) {
     const skills = Skill.getAll();
     res.render('skills/index', { skills });
+}
+
+function show(req, res) {
+    const param = parseInt(req.params.id);
+    const skill = Skill.getById(param);
+    res.render('skills/show', {skill});
 }
--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,21 @@
 const skills = [
-    {id: 12345, skills: 'Communication', done:true},
-    {id: 12346, skills: 'Poblem Solving', done:true},
-    {id: 12347, skills: 'Managment', done:true},
-    {id: 12348, skills: 'Planning', done:true}
+    {id: 12345, name: 'Communication', done:true},
+    {id: 12346, name: 'Poblem Solving', done:true},
+    {id: 12347, name: 'Managment', done:true},
+    {id: 12348, name: 'Planning', done:true}
 ]
 
 module.exports = {
-    getAll
+    getAll,
+    getById
 };
 
 function getAll() {
     return skills;
 }
+
+function getById(id) {
+    return skills.find(function(skill) {
+        return skill.id === id 
+    })
+}  
